d54acf818d81eac60b91947ec188234ea3232422
geekcoty <ccareal@gmail.com>
2020-12-06 07:44:18 -0300

    node exercises

---
+++ expresstest/routes/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", function (req, res, next) {
+  res.send("esto es un test"
+  );
+});
+//ejercicio6
+router.get("/ej3/:name/", function (req, res, next) {
+  res.send(
+    `El nombre del usuario es ${req.params.name} y tiene ${req.query.edad} años`
+  );
+});
+
+//ejercicio4
+router.get("/ej4/:num/", function (req, res, next) {
+
+  res.send(`${req.params.num*2}`);
+});
+
+  
+//ejercicio5
+router.get("/ej5/:str/", function (req, res, next) {
+
+let string = `${req.params.str}`
+let palin = string.split("").reverse().join("");
+if (palin === string) {res.send (string + " es un palindomo")}
+    else {res.send(string + " no es un palindtromo")
+}
+
+});
+
+module.exports = router;
