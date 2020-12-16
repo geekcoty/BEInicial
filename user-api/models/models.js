65fdab31c3fed9d54dc142882b438b94b6677dbb
geekcoty <ccareal@gmail.com>
2020-12-16 20:41:33 -0300

    more new files

---
+++ user-api/models/models.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const testSchema = mongoose.Schema({
+  testProperty: {
+    type: String,
+  },
+  anotherProperty: {
+    type: Number,
+    required: true,
+  },
+  yetAnotherProperty: [],
+});
+module.exports = mongoose.model("Test", testSchema);
