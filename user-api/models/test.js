4970e21cd5f74c009008981736c26646d6c10c40
geekcoty <ccareal@gmail.com>
2020-12-18 22:29:16 -0300

    new files

--- user-api/models/test.js
+++ user-api/models/test.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+
 const testSchema = mongoose.Schema({
   testProperty: {
     type: String,
--- user-api/routes/index.js
+++ user-api/routes/index.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send("Hola");
 });
 
+router.post ("/add", function (req,res,next){
+
+})
 module.exports = router;
