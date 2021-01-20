4514f0e7e40f67d5ed05eabf54534ac26a85f50d
geekcoty <ccareal@gmail.com>
2021-01-20 14:00:38 -0300

    initial commit

--- tp-back/app.js
+++ tp-back/app.js
@@ -6,18 +6,6 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 
 
-// Passport require
-const passportConfig = require("./passport");
-const passport = require("passport");
-const session = require("express-session");
-
-const sessionMiddleware = session({
-  name: "tp-back",
-  secret: "p4l4br4_m4g1k4",
-  saveUninitialized: false,
-  resave: false,
-});
-
 const app = express();
 
 app.use(logger('dev'));
@@ -25,11 +13,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(sessionMiddleware);
-
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 
--- tp-back/routes/index.js
+++ tp-back/routes/index.js
@@ -1,70 +1,9 @@
 const express = require('express');
-const passport = require("passport");
 const router = express.Router();
-
-const UserController = require ("./../controllers/userController")
-const UserService = require ("./../services/userService")
-const UserInstance = new UserController(new UserService())
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("Bienvenides al TP final de Back End");
+  res.render('index', { title: 'Express' });
 });
-
-/*PELICULAS
-
-//Muestra un array con todas las películas. Solo se puede acceder autenticado
-router.get("/movies", function (req, res, next) {
-  res.send("array de movies");
-});
-
-//Muestra la información de una película especīfica. Solo se puede acceder autenticado
-router.get("/movies/:id", function (req, res, next) {
-  res.send("Bienvenides al TP final de Back End");
-});
-
-//Sirve para crear una película en la base de datos. Necesita estar autenticado y ser admin para que se ejecute
-router.post("/movies", function (req, res, next) {
-  res.send("nueva peli");
-});
-
-//Sirve para modificar una película en la base de datos. Necesita estar autenticado y ser admin para que se ejecute
-router.put("/movies/edit/:id", function (req,res,next) {
-  res.send("modificar peliculas")
-});
-
-//Sirve para borrar una película de la base de datos. Necesita estar autenticado y ser admin para que se ejecute.
-
-router.delete("/movies/delete/:id", function(req,res,next) {
-  res.send("borrar peliculas")
-});*/
-
-//USUARIOS
-
-//Muestra una lista de usuarios, no tiene restricciones de acceso
-router.get("/users", function (req, res,next) {
-  UserInstance.getUser(req,res)
-});
-
-//Muestra la información de un usuario particular, no tiene restricciones de acceso
-router.get("/users/:id", function (req, res, next) {
-  UserInstance.findUser(req,res)
-});
-
-//Sirve para crear un usuario en la base de datos, no tiene restricciones de acceso
-router.post("/users/", function(req, res,next){
-  UserInstance.addUser(req,res)
-});
-
-//Sirve para modificar un usuario en la base de datos. Necesita estar autenticado y ser admin para que se ejecute
-router.put("/users/edit/:id", passport.authenticate("local"),function (req,res,next) {
-  UserInstance.editUser(req,res)
-
-})
-
-//Sirve para borrar un usuario de la base de datos. Necesita estar autenticado y ser admin para que se ejecute
-router.delete("/users/delete/:id", passport.authenticate("local") ,function (req,res,next){
-  UserInstance.deleteUser(req,res)
-})
 
 module.exports = router;
